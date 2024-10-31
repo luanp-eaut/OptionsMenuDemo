8da05f51cee419ccd79da704b192117487569e23
luanp-eaut <luanp@eaut.edu.vn>
2024-10-31 09:23:32 +0700

    first commit

---
+++ app/src/main/java/eaut/it/mobileappdev/optionsmenudemo/MainActivity.java
@@ -0,0 +1,49 @@
+package eaut.it.mobileappdev.optionsmenudemo;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        String selectedStr;
+        if(id==R.id.home) selectedStr = "Home Selected";
+        else if(id==R.id.setting) selectedStr="Settings Selected";
+        else selectedStr = "You have logged out";
+        Toast.makeText(this,selectedStr,Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
